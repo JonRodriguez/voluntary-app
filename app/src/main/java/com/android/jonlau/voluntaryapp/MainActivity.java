0ca66635f024f1833f130e1d61dd0eac6572978c
JonRodriguez <jon.rodriguezhidalgo@gmail.com>
2018-01-07 20:40:23 +0100

    added Profile Activity

--- app/src/main/java/com/android/jonlau/voluntaryapp/MainActivity.java
+++ app/src/main/java/com/android/jonlau/voluntaryapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.android.jonlau.voluntaryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.*;
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.profile_button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openProfileActivity(v);
+            }
+        });
+    }
+
+    public void openProfileActivity(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
