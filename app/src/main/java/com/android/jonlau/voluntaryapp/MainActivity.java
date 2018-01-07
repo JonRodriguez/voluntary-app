1144622a5da900867164ed23a762d5ea35ab9fc2
Laura Cervantes <cmlaura91@gmail.com>
2018-01-07 22:08:51 +0100

    Main buttons created

--- app/src/main/java/com/android/jonlau/voluntaryapp/MainActivity.java
+++ app/src/main/java/com/android/jonlau/voluntaryapp/MainActivity.java
@@ -14,10 +14,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.profile_button_id);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Intent profileIntent = new Intent(this, ProfileActivity.class);
+        final Intent chatIntent = new Intent(this, ChatActivity.class);
+        final Intent newsIntent = new Intent(this, NewsActivity.class);
+        final Intent gamesIntent = new Intent(this, GamesActivity.class);
+
+        final Button profileButton = findViewById(R.id.profile_button_id);
+        profileButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                openProfileActivity(v);
+                startActivity(profileIntent);
+            }
+        });
+        final Button chatButton = findViewById(R.id.chat_button_id);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(chatIntent);
+            }
+        });
+        final Button newsButton = findViewById(R.id.news_button_id);
+        newsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(newsIntent);
+            }
+        });
+        final Button gamesButton = findViewById(R.id.games_button_id);
+        gamesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(gamesIntent);
             }
         });
     }
